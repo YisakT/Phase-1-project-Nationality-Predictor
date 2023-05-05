1e755610ba3f0599d30e8d18e6be0fe6b77ab3cb
Yisak Girmay <yisakbruck23@gmail.com>
2023-05-04 20:17:55 -0400

    map over array of predicted nationalities

--- script.js
+++ script.js
@@ -6,4 +6,11 @@ document.getElementById("submit-button").onclick = function(event) {
     .then(data => {
         const predictions = data.country.map(prediction => {
             return fetch(`https://restcountries.com/v3.1/alpha/${prediction.country_id}`)
+            .then(response => response.json())
+                    .then(countryData => {
+                        const countryName = countryData[0].name.common;
+                        const capitalCity = countryData[0].capital[0];
+                        const flagUrl = countryData[0].flags.png;
+                        return {countryId: prediction.country_id, probability: prediction.probability, flagUrl: flagUrl, countryName: countryName, capitalCity: capitalCity};
+                    });
 }
