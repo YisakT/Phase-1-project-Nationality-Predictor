9415d46ff8da309e9fd7cfb60b6269fb0c2c33d7
Yisak Girmay <yisakbruck23@gmail.com>
2023-05-04 20:06:49 -0400

    add event lisner to submit-button

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
 document.getElementById("submit-button").onclick = function(event) {
     event.preventDefault();
-    const name = document.getElementById("input-name").value;}
+    const name = document.getElementById("input-name").value;
+    fetch(`https://api.nationalize.io?name=${name}`)
+    .then(response => response.json())
+}
