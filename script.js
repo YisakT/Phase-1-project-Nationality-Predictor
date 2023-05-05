c80595639da3e725902ef767695db8b16c9e4450
Yisak Girmay <yisakbruck23@gmail.com>
2023-05-04 20:15:22 -0400

    request api to get nationality

--- script.js
+++ script.js
@@ -3,4 +3,7 @@ document.getElementById("submit-button").onclick = function(event) {
     const name = document.getElementById("input-name").value;
     fetch(`https://api.nationalize.io?name=${name}`)
     .then(response => response.json())
+    .then(data => {
+        const predictions = data.country.map(prediction => {
+            return fetch(`https://restcountries.com/v3.1/alpha/${prediction.country_id}`)
 }
