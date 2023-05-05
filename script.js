fba8ad67e297f3d4b7fd57c0972ff1fcbdbfc753
Yisak Girmay <yisakbruck23@gmail.com>
2023-05-04 22:25:12 -0400

    catch when no result ar found

--- script.js
+++ script.js
@@ -5,15 +5,24 @@ document.getElementById("submit-button").onclick = function(event) {
     fetch(`https://api.nationalize.io?name=${name}`)
     .then(response => response.json())
     .then(data => {
-        const predictions = data.country.map(prediction => {
-            return fetch(`https://restcountries.com/v3.1/alpha/${prediction.country_id}`)
-            .then(response => response.json())
-                    .then(countryData => {
-                        const countryName = countryData[0].name.common;
-                        const capitalCity = countryData[0].capital[0];
-                        const flagUrl = countryData[0].flags.png;
-                        return {countryId: prediction.country_id, probability: prediction.probability, flagUrl: flagUrl, countryName: countryName, capitalCity: capitalCity};
-                    });
+        if (data.country.length === 0) {
+            const resultContainer = document.getElementById("result");
+            while (resultContainer.firstChild) {
+                resultContainer.removeChild(resultContainer.firstChild);
+            }
+            const noPredictionElement = document.createElement("p");
+            noPredictionElement.textContent = `No predictions found. Please check your entry for typos.`;
+            resultContainer.appendChild(noPredictionElement);
+        } else {
+            const predictions = data.country.map(prediction => {
+                return fetch(`https://restcountries.com/v3.1/alpha/${prediction.country_id}`)
+                .then(response => response.json())
+                .then(countryData => {
+                    const countryName = countryData[0].name.common;
+                    const capitalCity = countryData[0].capital[0];
+                    const flagUrl = countryData[0].flags.png;
+                    return {countryId: prediction.country_id, probability: prediction.probability, flagUrl: flagUrl, countryName: countryName, capitalCity: capitalCity};
+                });
             });
             Promise.all(predictions).then(predictions => {
                 const resultContainer = document.getElementById("result");
@@ -36,7 +45,8 @@ document.getElementById("submit-button").onclick = function(event) {
                     resultContainer.appendChild(predictionElement);
                 });
             });
-        });
+        }
+    });
 };
 
 document.getElementById("input-name").onkeydown = function(event) {
@@ -45,4 +55,3 @@ document.getElementById("input-name").onkeydown = function(event) {
         document.getElementById("submit-button").click();
     }
 };
-                  
